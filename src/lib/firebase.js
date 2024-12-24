d3145cfa9e006123568770242917d5dc9b0fa253
Francklin <francklinoklou@gmail.com>
2024-12-24 22:34:46 +0000

    Added .env to .gitignore and updated the Firestore file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,15 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
-
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBOK_yhT61H7NJKbFTSUcW_LPXmm6EWlPg",
-  authDomain: "newchatapp-fd9c7.firebaseapp.com",
-  projectId: "newchatapp-fd9c7",
-  storageBucket: "newchatapp-fd9c7.appspot.com",
-  messagingSenderId: "605578322292",
-  appId: "1:605578322292:web:0f838933ed7e1c09facc21",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
@@ -22,4 +22,3 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const rtdb = getDatabase(app);
-
